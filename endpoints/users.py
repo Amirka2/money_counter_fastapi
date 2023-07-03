45e66d945fe1c44b7e661ffb6f5f0f3f598448a3
amir_mac <far20022002@mail.ru>
2023-07-04 01:23:09 +0500

    microfix

--- endpoints/users.py
+++ endpoints/users.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, APIRouter
+from fastapi import APIRouter
 from models.models import User
 
 
--- models/models.py
+++ models/models.py
@@ -3,10 +3,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
 
 
-# строка подключения
 SQLALCHEMY_DATABASE_URL = "sqlite:///././db/sql_app.db"
 
-# создаем движок SqlAlchemy
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
