c136048046f27c369c7b2f5dd0e05357dd7a96d7
amir_mac <far20022002@mail.ru>
2023-07-04 01:37:35 +0500

    added endpoints for users

--- endpoints/users.py
+++ endpoints/users.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
 from db.models import User
+from db.sessions import get_session
 
 
 router = APIRouter(prefix="/users/")
@@ -7,9 +8,16 @@ router = APIRouter(prefix="/users/")
 
 @router.get("/")
 async def get_users():
-    return {"message": "Hello World"}
+    db = get_session()
+    
+    return db.Query(User).all()
 
 
 @router.post("/")
-async def add_user(name: str):
-    return {User}
+async def add_user(tg_id: int, first_name: str,
+                   last_name: str, username: str, hash: str):
+    db = get_session()
+    user = User(tg_id, first_name, last_name, username, hash)
+    db.add(user)
+
+    return user
