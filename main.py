035ae5e03af74a31293e60384d544aada3214863
amir_mac <far20022002@mail.ru>
2023-07-05 16:44:13 +0500

    changed ip

--- main.py
+++ main.py
@@ -69,7 +69,7 @@ app.mount("/", StaticFiles(directory="static", html=True), name="static")
 if __name__ == '__main__':
     uvicorn.run(
         "main:app",
-        host="0.0.0.0",
+        host="127.0.0.1",
         port=8080,
         reload=True
     )
